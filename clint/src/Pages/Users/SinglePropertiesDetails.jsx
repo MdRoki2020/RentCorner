53fc4d69eaec1b6d7a57192e70a6f29802f132c0
Roki <mroki815@gmail.com>
2023-06-26 23:30:54 +0600

    product viewer successful

--- clint/src/Pages/Users/SinglePropertiesDetails.jsx
+++ clint/src/Pages/Users/SinglePropertiesDetails.jsx
@@ -3,6 +3,7 @@ import { TbDetails } from 'react-icons/tb';
 import { ReadDataById } from '../../API Request/APIRequest';
 import { useParams } from 'react-router-dom';
 import '../../Assets/Styles/singlePropertiesDetails.css';
+import ReactImageMagnify from 'react-image-magnify';
 
 const SinglePropertiesDetails = () => {
   const { id } = useParams();
@@ -31,54 +32,79 @@ const SinglePropertiesDetails = () => {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="card CategoriesPosterWrapper animated flipInX my-4">
-            <div className="row">
-              <div className="col-sm-3">
-                <h3 className="mt-3 mx-3">
-                  <TbDetails />
-                </h3>
-              </div>
-              <div className="col-sm-9">
-                <div className="posterText">
-                  <h2>Details</h2>
-                  <p>Single Product Details</p>
+        <div className='row'>
+        <div className='col-md-12'>
+            <div className='card CategoriesPosterWrapper animated flipInX my-4'>
+            <div className='row'>
+                <div className='col-sm-2'>
+                <h2 className="mt-3 mx-3">
+                <TbDetails />
+                </h2>
                 </div>
-              </div>
+                <div className='col-sm-10'>
+                <div className='posterText'>
+                <h2>Details</h2>
+                <p>Single Properties Details</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+
+      <div className="row">
+        <div className="col-md-4">
+          <div className="product-image-viewer">
+            <div className="main-image-viewer img-fluid ">
+              {mainImage && (
+                <ReactImageMagnify
+                  {...{
+                    smallImage: {
+                      alt: 'productImage',
+                      isFluidWidth: true,
+                      src: mainImage,
+                    },
+                    largeImage: {
+                      src: mainImage,
+                      width: 1200,
+                      height: 1800,
+                    },
+                  }}
+                />
+              )}
+            </div>
+            <div className="small-image-viewers">
+              <img
+                className="img-fluid img-thumbnail"
+                src={firstImage}
+                alt="Image 0"
+                onClick={() => handleImageClick(firstImage)}
+              />
+              <img
+                className="img-fluid img-thumbnail"
+                src={secondImage}
+                alt="Image 1"
+                onClick={() => handleImageClick(secondImage)}
+              />
+              <img
+                className="img-fluid img-thumbnail"
+                src={thirdImage}
+                alt="Image 2"
+                onClick={() => handleImageClick(thirdImage)}
+              />
+              {/* Add more small images here */}
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="row">
-        <div className='col-md-4'>
-
-
-        <div className="product-image-viewer">
-          <div className="main-image-viewer">
-            {mainImage && <img className='img-fluid img-thumbnail' src={mainImage} alt="Main Image" />}
-          </div>
-          <div className="small-image-viewers">
-            <img
-              className='img-fluid img-thumbnail'
-              src={secondImage}
-              alt="Image 1"
-              onClick={() => handleImageClick(secondImage)}
-            />
-            <img
-              className='img-fluid img-thumbnail'
-              src={thirdImage}
-              alt="Image 2"
-              onClick={() => handleImageClick(thirdImage)}
-            />
-            {/* Add more small images here */}
-          </div>
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui nam
+            sapiente odio facere tenetur provident harum nihil, placeat eius
+            assumenda unde possimus quod, consectetur iste consequatur libero
+            eum sed dolor?
+          </p>
         </div>
-
-
-        </div>
-        <div className='col-md-7'></div>
       </div>
     </div>
   );
