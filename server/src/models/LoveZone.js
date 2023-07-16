2cc8d8931b543263f43d8fd0bf70e01ac1d253e3
Roki <mroki815@gmail.com>
2023-07-17 00:15:30 +0600

    update love zone Model

--- server/src/models/LoveZone.js
+++ server/src/models/LoveZone.js
@@ -2,6 +2,10 @@ const mongoose=require('mongoose');
 const LoveZoneSchema=mongoose.Schema({
     userEmail:{type:String},
     PropertiesId:{type:String},
+    image:{type:String},
+    category:{type:String},
+    HouseName:{type:String},
+    Status:{type:String},
     createdDate:{type:Date,default:Date.now()}
 });
 const LoveZoneModel=mongoose.model('LoveZones',LoveZoneSchema);
