66b85f7d974467f9653c483815e7fb8b17b56c3d
Roki <mroki815@gmail.com>
2023-07-30 00:31:09 +0600

    BookingModel update

--- server/src/models/BookingModel.js
+++ server/src/models/BookingModel.js
@@ -8,6 +8,7 @@ const BookingSchema=mongoose.Schema({
     userNid:{type:String},
     userimageUrl:{type:String},
     category:{type:String},
+    Status:{type:String},
     createdDate:{type:Date,default:Date.now()}
 });
 const BookingModel=mongoose.model('bookingRequests',BookingSchema);
