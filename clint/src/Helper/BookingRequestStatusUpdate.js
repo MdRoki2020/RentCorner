4e664593296f7a29e64eba28e707b8eae09f1d3c
Roki <mroki815@gmail.com>
2023-08-06 13:07:17 +0600

    status update issue fixed

--- clint/src/Helper/BookingRequestStatusUpdate.js
+++ clint/src/Helper/BookingRequestStatusUpdate.js
@@ -1,15 +1,29 @@
 import Swal from "sweetalert2";
 import { UpdateBookingRequestStatusRequest } from "../API Request/APIRequest";
 
-export function UpdateBookingRequestStatusToDO(id,status){
+export function UpdateBookingRequestStatusToDO(id, status) {
     return Swal.fire({
         title: 'Change Status',
         input: 'select',
-        inputOptions: {Available: 'Available',Booked: 'Booked'},
-        inputValue:status,
-    }).then((result)=>{
-        return UpdateBookingRequestStatusRequest (id, result.value).then((res)=>{
-            return res;
-        })
-    })
+        inputOptions: { Available: 'Available', Booked: 'Booked' },
+        inputValue: status,
+        showCancelButton: true,
+        cancelButtonText: 'Cancel',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (result.value !== undefined) {
+                return UpdateBookingRequestStatusRequest(id, result.value).then((res) => {
+                    return res;
+                }).catch((error) => {
+                    console.error("Error updating booking request status:", error);
+                    throw error;
+                });
+            }
+        } else {
+            return "Booking request status change cancelled";
+        }
+    }).catch((error) => {
+        console.error("Error in Swal.fire:", error);
+        throw error;
+    });
 }
--- clint/src/Helper/UpdateAlert.js
+++ clint/src/Helper/UpdateAlert.js
@@ -1,15 +1,31 @@
 import Swal from "sweetalert2";
 import { UpdateStatusRequest } from "../API Request/APIRequest";
 
-export function UpdateToDO(id,status){
+export function UpdateToDO(id, status) {
     return Swal.fire({
         title: 'Change Status',
         input: 'select',
-        inputOptions: {Available: 'Available',Booked: 'Booked'},
-        inputValue:status,
-    }).then((result)=>{
-        return UpdateStatusRequest (id, result.value).then((res)=>{
-            return res;
-        })
-    })
+        inputOptions: { Available: 'Available', Booked: 'Booked' },
+        inputValue: status,
+        showCancelButton: true,  // Show a cancel button
+        cancelButtonText: 'Cancel',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (result.value !== undefined) {
+                return UpdateStatusRequest(id, result.value).then((res) => {
+                    return res;
+                }).catch((error) => {
+                    console.error("Error updating status:", error);
+                    throw error;
+                });
+            }
+        } else {
+            return "Status change cancelled";
+        }
+    }).catch((error) => {
+        console.error("Error in Swal.fire:", error);
+        throw error;
+    });
 }
+
+
